24dc2f9935a19b1a4aff4caf482242a52fd14ac9
JuanPepas <finnbl15yt@gmail.com>
2023-11-08 11:18:03 -0500

    se añade la opcion de buscar el codigo del color

--- js/hex.js
+++ js/hex.js
@@ -1,4 +1,4 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A",
+/*const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A",
 "B", "C", "D", "E", "F"];
 // #f15025
 const btn = document.getElementById("btn");
@@ -15,4 +15,40 @@ btn.addEventListener('click', function() {
 
 function getRandomNumber(){
     return Math.floor(Math.random() * hex.length);
-}
+}*/
+
+const hex = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"
+  ];
+  
+  function buscarColor() {
+      const searchInput = document.getElementById("searchInput").value;
+      const colorElement = document.querySelector(".color");
+      const resultadosDiv = document.getElementById("resultadosBusqueda");
+  
+      const isValidHex = /^#[0-9A-F]{6}$/i.test(searchInput);
+  
+      if (isValidHex) {
+          colorElement.textContent = searchInput;
+          document.body.style.backgroundColor = searchInput;
+          resultadosDiv.innerHTML = `<p>Color code: ${searchInput}</p>`;
+      } else {
+          alert("Please enter a valid hexadecimal color code (e.g., #00FF00).");
+      }
+  }
+  
+  const btn = document.getElementById("btn");
+  btn.addEventListener('click', function() {
+      let hexColor = '#';
+      for (let i = 0; i < 6; i++) {
+          hexColor += hex[getRandomNumber()];
+      }
+      document.querySelector(".color").textContent = hexColor;
+      document.body.style.backgroundColor = hexColor;
+      document.getElementById("resultadosBusqueda").innerHTML = `<p>Color code: ${hexColor}</p>`;
+  });
+  
+  function getRandomNumber() {
+      return Math.floor(Math.random() * hex.length);
+  }
+  
--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-const colors = ["green","red","rgba(133,122,200)",
+/*const colors = ["green","red","rgba(133,122,200)",
 "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener('click',function () {
-    //ger random number between 0 - 3 
+    //get random number between 0 - 3 
     const randomNumber = getRandomNumber();
     console.log(randomNumber);
     document.body.style.backgroundColor = colors[randomNumber];
@@ -13,4 +13,33 @@ btn.addEventListener('click',function () {
 
 function getRandomNumber(){
     return Math.floor (Math.random() * colors.length);
+}*/
+
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+function buscarColor() {
+    const searchInput = document.getElementById("searchInput").value;
+    const colorElement = document.querySelector(".color");
+    const resultadosDiv = document.getElementById("resultadosBusqueda");
+  
+    if (colors.includes(searchInput)) {
+        document.body.style.backgroundColor = searchInput;
+        colorElement.textContent = searchInput;
+        resultadosDiv.innerHTML = `<p>Color code: ${searchInput}</p>`;
+    } else {
+        alert("Color not found in the list!");
+    }
 }
+
+const btn = document.getElementById("btn");
+btn.addEventListener('click', function () {
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    document.querySelector(".color").textContent = colors[randomNumber];
+    document.getElementById("resultadosBusqueda").innerHTML = `<p>Color code: ${colors[randomNumber]}</p>`;
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
+
